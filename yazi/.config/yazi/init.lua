a9a5029b4cf3317e9d57908ffef81519344cccff
dapp-whisperer <dapp-whisperer@pm.me>
2026-02-18 19:35:35 -0600

    updates

--- yazi/.config/yazi/init.lua
+++
@@ -1,8 +0,0 @@
--- Disable mouse click actions in Yazi.
---
--- Using `reveal` here can change the current directory when the clicked item
--- is outside the active folder (for example, parent pane items), which feels
--- like random directory jumps.
-function Entity:click(event, up)
-  return
-end
